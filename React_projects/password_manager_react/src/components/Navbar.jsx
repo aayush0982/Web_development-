4a7c4cc08d7975d4e0dfb4e1f8c1d5e4b5a3d891
aayush saxena <aayushsaxena@aayushs-MacBook-Air.local>
2025-01-15 03:04:06 +0530

    passowrd manager

---
+++ React_projects/password_manager_react/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='nav'>
+      <img src="key.svg" width={32} alt="logo"/>
+      <span className='navtitle'>OpManager</span>
+    </div>
+  )
+}
+
+export default Navbar
